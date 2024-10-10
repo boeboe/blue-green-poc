6c0dd995af6d11782936abeedcc3d64523e97778
Bart Van Bos <bart@tetrate.io>
2024-10-10 07:34:47 +0200

    added date created and updated fields

--- services/msg-sender-backend/src/main/java/net/liantis/messagesender/model/Message.java
+++ services/msg-sender-backend/src/main/java/net/liantis/messagesender/model/Message.java
@@ -1,14 +1,28 @@
 package net.liantis.messagesender.model;
 
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class Message {
   private int id;
   private String topic;
   private String message;
 
-  public Message(int id, String topic, String message) {
+  
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+  private LocalDateTime dateCreated;
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+  private LocalDateTime dateUpdated;
+
+  // Constructor with dates
+  public Message(int id, String topic, String message, LocalDateTime dateCreated, LocalDateTime dateUpdated) {
     this.id = id;
     this.topic = topic;
     this.message = message;
+    this.dateCreated = dateCreated;
+    this.dateUpdated = dateUpdated;
   }
 
   // Getters and setters
@@ -34,5 +48,21 @@ public class Message {
 
   public void setMessage(String message) {
     this.message = message;
+  }
+
+  public LocalDateTime getDateCreated() {
+    return dateCreated;
+  }
+
+  public void setDateCreated(LocalDateTime dateCreated) {
+    this.dateCreated = dateCreated;
+  }
+
+  public LocalDateTime getDateUpdated() {
+    return dateUpdated;
+  }
+
+  public void setDateUpdated(LocalDateTime dateUpdated) {
+    this.dateUpdated = dateUpdated;
   }
 }
